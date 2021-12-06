bbf01328f94c4880f3a2e53855e9849e9e4becbc
cab-aileen <cabreraaileen3@gmail.com>
2021-12-06 13:09:40 -0500

    wingsflap

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,19 +8,30 @@ import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
 gsap.registerPlugin(GSDevTools, MorphSVGPlugin, DrawSVGPlugin, Flip, MotionPathHelper, MotionPathPlugin);
 
+gsap.set("#left-wing-7",{transformOrigin:"bottom"});
+gsap.set("#right-wing-9", {transformOrigin:"top"});
+gsap.set("#small-full-earth", {transformOrigin:"center"});
+
+
 
 const mainTL = gsap.timeline();
 
 
-
 function worldrotateandscale(){
     const tl =gsap.timeline();
-    tl.to("#small-full-earth",{duration:1,scale:3})
+    tl.to("#small-full-earth",{duration:2,scale:5.4})
+    
     return tl; 
 }
 
 function sidedragon(){
     const tl =gsap.timeline();
+    tl.from("#left-dragon",{duration:3, x:"-=1300"},"sametime")
+    //.to("#left-dragon",{duration:3.5,x:"-=-2200"},"-=1.5")
+    .to("#left-wing-7",{rotation:-55,repeat: -1,yoyo: true},"sametime")
+    .to("#right-wing-9",{rotation:55,repeat: -1,yoyo: true},"sametime")
+    .to("#right-tails", {duration:1,morphSVG:"#left-tails"})
+    
 
     return tl;
     
